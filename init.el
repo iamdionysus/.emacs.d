9da7332eee9a21e0ab8a4b02c33f8d28fa82b078
iamdionysus <iamdionysus@gmail.com>
2014-11-10 15:51:11 +0900

    smartparens added

--- init.el
+++ init.el
@@ -29,10 +29,9 @@
 ;;
 
 (menu-bar-mode -1) ;; no menu bar
-
 (setq compilation-scroll-output t)  ;; automatically go to bottom of the compilation buffer
 (setq make-backup-files nil) ;; no backup file ~
-
+(column-number-mode t)
 
 ;;; buffer-move
 ;;
@@ -272,6 +271,16 @@
 )
 
 
+;;; smartparens
+(use-package smartparens
+  :init
+  (progn
+    (require 'smartparens-config)
+    (smartparens-global-mode t)
+  )
+  :ensure t
+)
+
 
 ;;; setups for windows
 ;;
@@ -311,17 +320,3 @@
 
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
