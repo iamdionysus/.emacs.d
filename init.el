78b5ad8f6310079f9c4370d0fc7f3ad7e833ce43
iamdionysus <iamdionysus@gmail.com>
2014-12-16 11:47:47 +0800

    'ocaml set up added'

--- init.el
+++ init.el
@@ -286,6 +286,7 @@
 
 
 ;;; smartparens
+;;
 (use-package smartparens
   :init
   (progn
@@ -295,6 +296,37 @@
   :ensure t
 )
 
+
+;;; tuareg
+;;
+(use-package tuareg
+  :ensure t
+  :init
+  (progn
+    (autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
+    (add-hook 'tuareg-mode-hook 'utop-setup-ocaml-buffer)
+    (add-hook 'tuareg-mode-hook 'merlin-mode)
+  )
+)
+
+;;; utop
+;;
+(use-package utop
+  :ensure t
+)
+
+;;; merlin - needs opam install merlin
+;;
+;; set opam-share variable to user's .opam share
+(setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
+;; emacs related codes are in /emacs/site-lisp and load them all
+(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+(require 'merlin)
+
+;;; ocp-indent - needs opam install ocp-indent
+;;
+;; ocp-indent.el is already loaded from the code above
+(require 'ocp-indent)
 
 ;;; setups for windows
 ;;
