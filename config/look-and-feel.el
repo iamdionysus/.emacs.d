a12ba5a21da1b98ffee55121cf5fcdcb1ca11743
iamdionysus <iamdionysus@gmail.com>
2014-12-19 23:40:02 +0800

    initial version done

---
+++ config/look-and-feel.el
@@ -0,0 +1,46 @@
+;;; look-and-feel.el --- sets my emacs look&feel
+
+;;; Commentary;
+;;
+;; this file is to set up my own Emacs look&feel
+;; over mac, linux, windows envrionments
+
+;;; Code:
+
+(provide 'look-and-feel)
+
+(menu-bar-mode -1) ;; no menu bar
+(setq compilation-scroll-output t)  ;; automatically go to bottom of the compilation buffer
+(column-number-mode t)
+
+;;; setups for windows
+;;
+(if (eq system-type 'windows-nt)
+  (progn  
+    (tool-bar-mode -1) ;; no tool bar
+    (add-to-list 'initial-frame-alist '(height . 50))
+    (add-to-list 'initial-frame-alist '(width . 180))
+    (add-to-list 'initial-frame-alist '(left . 0))
+    (add-to-list 'initial-frame-alist '(top . 50))
+    (set-language-environment "UTF-8")
+    (set-face-attribute 'default nil :font "Consolas-12")
+    (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/")
+    (setq ispell-program-name "aspell")
+    (setq ispell-personal-dictionary "C:/Program Files (x86)/Aspell/dict")
+  )
+)
+
+;;; settings for mac osx
+;;
+(if (eq system-type 'darwin)
+  (progn
+    (tool-bar-mode -1)
+    (add-to-list 'initial-frame-alist '(height . 54))
+    (add-to-list 'initial-frame-alist '(width . 200))
+    (add-to-list 'initial-frame-alist '(left . 0))
+    (add-to-list 'initial-frame-alist '(top . 10))
+    (set-face-attribute 'default nil :font "Monaco-14")
+  )
+  )
+
+;;; look-and-feel.el ends here
--- init.el
+++ init.el
@@ -11,59 +11,39 @@
 
 ;;; Commonn-lisp compatibility
 ;;
-(with-no-warnings
-  (require 'cl))
+(with-no-warnings (require 'cl))
 
-;;; Look and feel
-;;
-
-(menu-bar-mode -1) ;; no menu bar
-(setq compilation-scroll-output t)  ;; automatically go to bottom of the compilation buffer
 (setq make-backup-files nil) ;; no backup file ~
-(column-number-mode t)
-
-;;; better deletion
-;;
-(global-set-key (kbd "C-w") 'backward-kill-word)
-(global-set-key (kbd "C-x C-k") 'kill-region)
-
-;;; manage window splits easily
-;;
-(global-set-key (kbd "C-x -") 'split-window-below)
-(global-set-key (kbd "C-x \\") 'split-window-right)
-(global-set-key (kbd "C-M-o") 'other-window)
-(global-set-key (kbd "C-M-k") 'shrink-window-horizontally)
-(global-set-key (kbd "C-M-l") 'enlarge-window-horizontally)
-(global-set-key (kbd "C-M-u") 'enlarge-window)
-(global-set-key (kbd "C-M-y") 'shrink-window)
-
-;;; load-file
-;;
-(global-set-key (kbd "C-x C-l") 'load-file)
 
 ;;; el-get settings
 ;;
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 (unless (require 'el-get nil 'noerror)
-  (require 'package)
-  (add-to-list 'package-archives
-	       '("melpa" . "http://melpa.org/packages/"))
-  (package-refresh-contents)
-  (package-initialize)
-  (package-install 'el-get)
-  (require 'el-get))
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
-(add-to-list 'el-get-recipe-path "~/.emacs.d/recipes/")
+(add-to-list 'el-get-recipe-path "~/.emacs.d/recipes")
 (setq el-get-verbose t)
 
-;; my package list goes here, group close ones
+;;; my package list goes here, group it
+;;
 (setq gabriel-packages
       (append
        '(smex multi-term
 	 smartparens
-	 web-mode
+	 web-mode emmet-mode sass-mode markdown-mode yaml-mode
 	 company-mode company-inf-ruby
-	 helm projectile)))
+	 helm projectile flx
+	 inf-ruby)))
 
 (el-get 'sync gabriel-packages)
+
+;;; custom settings
+;;
+(add-to-list 'load-path "~/.emacs.d/config")
+(require 'shortcuts)
+(require 'look-and-feel)
