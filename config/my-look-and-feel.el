bcd27918ba12d1d9bc13cfb17f07947b04f3ec2e
iamdionysus <iamdionysus@gmail.com>
2015-03-23 09:49:49 +0800

    changed windows look and feed

--- config/my-look-and-feel.el
+++ config/my-look-and-feel.el
@@ -14,10 +14,10 @@
 (if (eq system-type 'windows-nt)
   (progn  
     (tool-bar-mode -1) ;; no tool bar
-    (add-to-list 'initial-frame-alist '(height . 50))
-    (add-to-list 'initial-frame-alist '(width . 180))
+    (add-to-list 'initial-frame-alist '(height . 53))
+    (add-to-list 'initial-frame-alist '(width . 205))
     (add-to-list 'initial-frame-alist '(left . 0))
-    (add-to-list 'initial-frame-alist '(top . 50))
+    (add-to-list 'initial-frame-alist '(top . 0))
     (set-language-environment "UTF-8")
     (set-face-attribute 'default nil :font "Source Code Pro-11")
     (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/")
