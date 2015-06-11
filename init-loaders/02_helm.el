41736a5d43b87a64debbe0ee0d845bb596d8d04e
iamdionysus <iamdionysus@gmail.com>
2015-06-11 14:45:37 +0800

    reorganize

--- init-loaders/02_helm.el
+++ init-loaders/02_helm.el
@@ -5,12 +5,11 @@
 
 ;;; projectile
 ;;
-
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
 (setq projectile-indexing-method 'alien)
-;; (global-set-key (kbd "C-x p") 'helm-projectile-find-file)
+(global-set-key (kbd "C-x p") 'helm-projectile-find-file)
 (global-set-key (kbd "C-x i") 'projectile-invalidate-cache)
 ;; (setq projectile-switch-project-action 'helm-projectile)
 
