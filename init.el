df840a98bc1260390f8715e23d3e7fd9f5440332
iamdionysus <iamdionysus@gmail.com>
2015-06-16 15:31:27 +0800

    working with el-get-bundle now

--- init.el
+++ init.el
@@ -14,15 +14,43 @@
 (require 'el-get)
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 
+;;; essential
+;;
 (el-get-bundle init-loader)
 (el-get-bundle projectile)
 (el-get-bundle init-loader)
 (el-get-bundle golden-ratio)
 (el-get-bundle org)
+(el-get-bundle elpa:evil-nerd-commenter)
 
 (el-get-bundle projectile)
 (el-get-bundle helm)
 (el-get-bundle helm-projectile)
+
+(el-get-bundle company)
+(el-get-bundle ag)
+(el-get-bundle elpa:flycheck)
+
+;;; modes
+;;
+(el-get-bundle scala-mode2)
+(el-get-bundle web-mode)
+(el-get-bundle emmet-mode)
+(el-get-bundle markdown-mode)
+(el-get-bundle yaml-mode)
+(el-get-bundle coffee-mode)
+(el-get-bundle json-mode)
+
+;;; clojure
+;;
+(el-get-bundle cider)
+
+;;; ruby
+;;
+(el-get-bundle elpa:rinari)
+(el-get-bundle robe)
+(el-get-bundle ruby-tools)
+(el-get-bundle inf-ruby)
 
 (require 'init-loader)
 (custom-set-variables '(init-loader-show-log-after-init 'error-only))
