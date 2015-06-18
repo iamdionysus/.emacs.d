c8044ef50104b0d1b0d46fe299b7162596346bc8
iamdionysus <iamdionysus@gmail.com>
2015-06-18 10:38:14 +0800

    working on el-get

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 ;;
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (require 'el-get)
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(add-to-list 'el-get-recipe-path "~/.emacs.d/recipes")
 
 ;;; essential
 ;;
