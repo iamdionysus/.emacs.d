e028c03a426b3d7de9dda2c299e136e251966f43
iamdionysus <iamdionysus@gmail.com>
2015-07-07 15:19:31 +0800

    elisp helper function to make clojure doc better

--- init-loaders/23_clojure.el
+++ init-loaders/23_clojure.el
@@ -1,2 +1,20 @@
 (use-package cider
   :ensure t)
+
+;;; To make example easy
+;; (+ 3 5)
+;;=> 8
+(defun cider-eval-last-sexp-and-make-example ()
+  "Evaluate the sexp, comment it and append result next line."
+  (interactive)
+  (let ((last-sexp (cider-last-sexp)))
+    (backward-sexp)
+    (mark-sexp)
+    (paredit-comment-dwim)
+    (end-of-line)
+    (newline)
+    (insert ";;=> ")
+    (cider-interactive-eval last-sexp
+			    (cider-eval-print-handler))))
+
+(global-set-key (kbd "C-x e") 'cider-eval-last-sexp-and-make-example)
