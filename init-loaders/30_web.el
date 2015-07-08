0c4e371cfdb21e8752045ae19b2602a889ac1b01
iamdionysus <iamdionysus@gmail.com>
2015-07-08 13:54:19 +0800

    updated web-mode and emmet-mode

--- init-loaders/30_web.el
+++ init-loaders/30_web.el
@@ -1,54 +1,43 @@
 ;;; web-mode
 ;;
-(defun web-mode-inside-next-element()
-  "move to next html element and select element content"
-  (interactive)
-  (web-mode-element-end)
-  (web-mode-element-next)
-  (web-mode-element-content-select))
+(use-package web-mode
+  :ensure t
+  :mode ("\\.hbs\\'" "\\.erb\\'" "\\.html?\\'")
+  :init
+  (defun my-web-mode-hook ()
+      "Hooks for web mode"
+      (setq web-mode-markup-indent-offset 2)
+      (setq web-mode-css-indent-offset 2)
+      (setq web-mode-code-indent-offset 2)
+      (setq web-mode-enable-css-colorization t))
+  (add-hook 'web-mode-hook 'my-web-mode-hook)
+  :config
+  (defun web-mode-inside-next-element ()
+    "move to next html element and select element content"
+    (interactive)
+    (web-mode-element-end)
+    (web-mode-element-next)
+    (web-mode-element-content-select))
+  (defun web-mode-inside-previous-element ()
+    "move to next html element and select element content"
+    (interactive)
+    (web-mode-element-beginning)
+    (web-mode-element-previous)
+    (web-mode-element-content-select))
+  (define-key web-mode-map (kbd "C-M-n") 'web-mode-inside-next-element)
+  (define-key web-mode-map (kbd "C-M-p") 'web-mode-inside-previous-element))
 
-(defun web-mode-inside-previous-element()
-  "move to next html element and select element content"
-  (interactive)
-  (web-mode-element-beginning)
-  (web-mode-element-previous)
-  (web-mode-element-content-select))
-
-(eval-after-load 'web-mode
-  '(progn
-     (define-key web-mode-map (kbd "C-M-n") 'web-mode-inside-next-element)
-     (define-key web-mode-map (kbd "C-M-p") 'web-mode-inside-previous-element)))
+;;; emmet-mode
+;;
+(use-package emmet-mode
+  :ensure t
+  :init
+  (add-hook 'web-mode-hook 'emmet-mode))
 
 ;;; js-mode
 ;;
 (setq js-indent-level 2)
 
-;;; web-mode
-;;
-(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-;; (setq web-mode-comment-style 2)
-;; (setq web-mode-engines-alist
-;; 	  '(("ctemplate" . "\\.hbs\\'")
-;; 	    ("erb" . "\\.erb\\'"))
-
-(defun my-web-mode-hook ()
-  "Hooks for web mode."
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
-(add-hook 'web-mode-hook 'my-web-mode-hook)
-
-;;; emmet-mode
-;;
-(add-hook 'html-mode-hook 'emmet-mode)
-(add-hook 'web-mode-hook 'emmet-mode)
 
 ;;; coffee-mode
 ;;
