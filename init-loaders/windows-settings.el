c8f09d9b8287e705bc86a17fd627bbfaa2b4118d
iamdionysus <iamdionysus@gmail.com>
2015-07-18 01:20:15 +0900

    correct startup size for windows

--- init-loaders/windows-settings.el
+++ init-loaders/windows-settings.el
@@ -1,13 +1,21 @@
 (server-start)
-(if (eq system-type 'windows-nt)
-    (setq w32-get-true-file-attributes nil))
+(setq w32-get-true-file-attributes nil)
+(scroll-bar-mode -1)
+;;; font should be set first to get frame-char-width, line-pixel-height correct
+(set-face-attribute 'default nil :font "Source Code Pro-11")
 
-(add-to-list 'initial-frame-alist '(height . 53))
-(add-to-list 'initial-frame-alist '(width . 205))
+(let ((max-height (/ (display-pixel-height) (line-pixel-height)))
+      (max-width (/ (display-pixel-width) (frame-char-width)))
+      ;; windows task bar
+      (height-delta 3)
+      ;; windows outer frame
+      (width-delta 4))
+  (add-to-list 'initial-frame-alist (cons 'height (- max-height height-delta)))
+  (add-to-list 'initial-frame-alist (cons 'width (- max-width width-delta))))
+
 (add-to-list 'initial-frame-alist '(left . 0))
 (add-to-list 'initial-frame-alist '(top . 0))
 (set-language-environment "UTF-8")
-(set-face-attribute 'default nil :font "Source Code Pro-11")
 (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/")
 (setq ispell-program-name "aspell")
 (setq ispell-personal-dictionary "C:/Program Files (x86)/Aspell/dict")
@@ -17,6 +25,3 @@
   :ensure t
   :init
   (setq powershell-indent 2))
-
-;; (setq explicit-shell-file-name "c:\\windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe")
-;; (setq explicit-powershell.exe-args '("-Command" "-" ))
