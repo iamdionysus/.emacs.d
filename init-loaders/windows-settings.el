6072cd6be5961abd8893ada4d1740ba48ce9dbe2
iamdionysus <iamdionysus@gmail.com>
2015-07-20 08:49:33 +0800

    multi screen resolution handinling

--- init-loaders/windows-settings.el
+++ init-loaders/windows-settings.el
@@ -4,8 +4,12 @@
 ;;; font should be set first to get frame-char-width, line-pixel-height correct
 (set-face-attribute 'default nil :font "Source Code Pro-11")
 
-(let ((max-height (/ (display-pixel-height) (line-pixel-height)))
-      (max-width (/ (display-pixel-width) (frame-char-width)))
+(let ((max-height (/
+		   (if (> (display-pixel-height) 1080) 1080 (display-pixel-height))
+		   (line-pixel-height)))
+      (max-width (/
+		  (if (> (display-pixel-width) 1920) 1920 (display-pixel-width))
+		  (frame-char-width)))
       ;; windows task bar
       (height-delta 3)
       ;; windows outer frame
