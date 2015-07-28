040db8db13d0a61760084d5309c93c5e9c0067af
iamdionysus <iamdionysus@gmail.com>
2015-07-28 10:20:38 +0800

    added golden ratio exception

--- init-loaders/04_ui.el
+++ init-loaders/04_ui.el
@@ -1,6 +1,12 @@
 (use-package golden-ratio
   :ensure t
-  :config (golden-ratio-mode t))
+  :init
+  (setq split-width-threshold nil)
+  (setq golden-ratio-exclude-modes '("dired-mode"
+				     "cider-repl-mode"
+				     "cider-stacktrace-mode"))
+  :config
+  (golden-ratio-mode t))
 
 (use-package powerline
   :ensure t
